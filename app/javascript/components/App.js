06c7e101bf63364e3e5cce7ec1dfea3f4c259366
Goran SD <g.cvetic23@gmail.com>
2022-11-08 15:50:07 -0800

    additional react scrypt file

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,12 +1,10 @@
-import React from "react"
-import PropTypes from "prop-types"
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import PropTypes from "prop-types";
 class App extends React.Component {
-  render () {
-    return (
-      <React.Fragment>
-      </React.Fragment>
-    );
+  render() {
+    return <React.Fragment></React.Fragment>;
   }
 }
 
-export default App
+export default App;
