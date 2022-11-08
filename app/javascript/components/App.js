cf6b10947523d5e1dc736edf4f5fde2051426ec0
Goran SD <g.cvetic23@gmail.com>
2022-11-08 15:53:55 -0800

    default GET route removed

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import PropTypes from "prop-types";
 class App extends React.Component {
   render() {
-    return <React.Fragment></React.Fragment>;
+    return <React.Fragment>WELCOME TO GREGS LIST</React.Fragment>;
   }
 }
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'home/index'
   devise_for :users
   get '*path', to: 'home#index', constraints: ->(request){ request.format.html? }
   root 'home#index'
