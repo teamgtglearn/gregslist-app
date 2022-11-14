cf4604bfd1430b3b57e5d777b9a384aa76d7cee9
guanhchan <guanh.chan@gmail.com>
2022-11-14 15:13:14 -0800

    rspec for index

--- spec/requests/products_spec.rb
+++ spec/requests/products_spec.rb
@@ -2,6 +2,11 @@ require 'rails_helper'
 
 RSpec.describe "Products", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+
+    it "gets all the products" do
+      get "/products"
+      expect(response).to have_http_status(200)
+
+    end
   end
 end
