13f72c0c0d91031503fa811cc8a903070226e2a0
guanhchan <guanh.chan@gmail.com>
2022-11-15 10:03:59 -0800

    backend create func w/ strong params and r spec test

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,5 +2,22 @@ class ProductsController < ApplicationController
     def index
         products = Product.all
         render json: products
+    end
+
+    def create
+      product = Product.create(product_params)
+      if product.valid?
+        render json: product
+      else
+        render json: product.errors, status: 422
       end
+    end
+
+
+    private
+
+    def product_params
+      params.required(:product).permit(:title, :description, :price, :image, :user_id)
+    end
+    
 end
