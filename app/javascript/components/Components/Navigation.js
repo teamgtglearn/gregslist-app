42e50643ad0553f980a224f391b4f6ea168e00ba
Goran SD <g.cvetic23@gmail.com>
2022-11-16 15:22:44 -0800

    home page nav and login form navigation updated

--- app/javascript/components/Components/Navigation.js
+++ app/javascript/components/Components/Navigation.js
@@ -1,37 +1,74 @@
 import React from "react";
-import { Nav, NavItem } from "reactstrap";
+import { Nav, NavItem, Col, Button } from "reactstrap";
 import { NavLink } from "react-router-dom";
 
-const Navigation = () => {
+const Navigation = ({
+  logged_in,
+  current_user,
+  new_user_route,
+  sign_in_route,
+  sign_out_route,
+}) => {
   return (
-    <Nav style={{ maxHeight: "100%" }}>
-      <NavItem>
-        <NavLink
-          to="/"
-          className="py-0 px-3"
-          style={{ color: "black", fontSize: "small" }}
+    <Nav style={{ minWidth: "100%" }}>
+      <Col>
+        <NavItem>
+          <NavLink
+            to="/"
+            className="py-0 px-3"
+            style={{ color: "black", fontSize: "small" }}
+          >
+            Home
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink
+            to="/index"
+            className="py-0 px-3"
+            style={{ color: "black", fontSize: "small" }}
+          >
+            All Products
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink
+            to="/about"
+            className="py-0 px-3"
+            style={{ color: "black", fontSize: "small" }}
+          >
+            About Us
+          </NavLink>
+        </NavItem>
+      </Col>
+      <Col className="d-flex justify-content-between align-items-center">
+        <Button
+          color="success"
+          size="m"
+          style={{ height: "2.3rem", minWidth: "6rem", fontSize: "0.7rem" }}
         >
-          Home
-        </NavLink>
-      </NavItem>
-      <NavItem>
-        <NavLink
-          to="/index"
-          className="py-0 px-3"
-          style={{ color: "black", fontSize: "small" }}
+          <NavItem>
+            <a id="login-btns" href={sign_in_route} className="nav-link">
+              Sign In
+            </a>
+          </NavItem>
+        </Button>
+        <Button
+          color="success"
+          size="m"
+          style={{
+            height: "2.3rem",
+            minWidth: "6rem",
+            fontSize: "0.7rem",
+            marginInlineStart: "1.5rem",
+          }}
         >
-          All Products
-        </NavLink>
-      </NavItem>
-      <NavItem>
-        <NavLink
-          to="/about"
-          className="py-0 px-3"
-          style={{ color: "black", fontSize: "small" }}
-        >
-          About Us
-        </NavLink>
-      </NavItem>
+          <NavItem>
+            <a id="login-btns" href={new_user_route} className="nav-link">
+              Sign Up
+            </a>
+          </NavItem>
+        </Button>
+      </Col>
     </Nav>
   );
 };
